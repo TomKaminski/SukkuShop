708ba0fa70b4341b37891c138f84d52281abf8d6
TomKaminski <tkaminski93@gmail.com>
2014-10-22 20:29:07 +0200

    Commit

--- SukkuShop/App_Start/BundleConfig.cs
+++ SukkuShop/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace SukkuShop
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+                "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                "~/Scripts/main.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
---
+++ SukkuShop/Scripts/main.js
@@ -0,0 +1,17 @@
+﻿//$(document).ready(function () {
+//    var stickyNavTop = $('.nav-low').offset().top;
+//    $('.sticky').hide();
+//    var stickyNav = function () {
+//        var scrollTop = $(window).scrollTop();
+//        if (scrollTop > stickyNavTop) {
+//            $('.nav-low').slideUp('slow');
+//            $('.sticky').slideDown('slow');
+//        } else {
+//            $('.nav-low').slideDown('slow');
+//            $('.sticky').slideUp('slow');
+//        }
+//    };
+//    $(window).scroll(function () {
+//        stickyNav();
+//    });
+//});
