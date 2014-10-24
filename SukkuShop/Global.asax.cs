aa9288cdf27fa6f13859fa0dc27dc32161c51720
TomKaminski <tkaminski93@gmail.com>
2014-10-24 11:41:44 +0200

    Layout fix/Filter update

--- SukkuShop/Global.asax.cs
+++ SukkuShop/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SukkuShop.Infrastructure.Binders;
 using SukkuShop.Models;
 
 namespace SukkuShop
@@ -15,6 +16,7 @@ namespace SukkuShop
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             Database.SetInitializer(new ApplicationDbContext.DropCreateInitializer());
+            ModelBinders.Binders.Add(typeof(ProductFilter), new ProductFilterBinder());
         }
     }
 }
---
+++ SukkuShop/Infrastructure/Binders/ProductFilterBinder.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+
+namespace SukkuShop.Infrastructure.Binders
+{
+    public enum SortMethod
+    {
+        FromHighestPrice,
+        FromLowestPrice,
+        Popularnosc,
+        Promotion,
+        Novelty
+    }
+
+    public class ProductFilter
+    {
+        public string Fraza { get; set; }
+        public string[] SelectedCategories { get; set; }
+        public int CenaMax { get; set; }
+        public int CenaMin { get; set; }
+        public bool Available { get; set; }
+        public SortMethod SortMethod { get; set; }
+    }
+
+    public class ProductFilterViewModel
+    {
+        public string Fraza { get; set; }
+        public string[] SelectedCategories { get; set; }
+        public int CenaMax { get; set; }
+        public int CenaMin { get; set; }
+        public bool Available { get; set; }
+        public SortMethod SortMethod { get; set; }
+    }
+    public class ProductFilterBinder : IModelBinder
+    {
+        private const string SessionName = "ProductFilter";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var filter = controllerContext.HttpContext.Session[SessionName] as ProductFilter ?? new ProductFilter();
+            controllerContext.HttpContext.Session[SessionName] = filter;
+            return filter;
+        }
+    }
+}
