22c5a14cd32cbcc9248944b5e8cc4e5fda9bf442
kovson <justmitli@gmail.com>
2014-11-17 13:38:33 +0100

    Cart asd

---
+++ SukkuShop/Models/CartViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SukkuShop.Models
+{
+    public class CartViewModels
+    {
+        public List<CartProduct> CartProductsList {get; set;}
+        public decimal TotalValue { get; set; }
+    }
+
+    public class CartProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Price { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
