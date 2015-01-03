1908cafb0549af0adca15642838ca6c1ba39a4d0
TomKaminski <tkaminski93@gmail.com>
2015-01-03 20:43:57 +0100

    db fix

--- SukkuShop/Migrations/201501031003544_UserHints.cs
+++ SukkuShop/Migrations/201501031003544_UserHints.cs
@@ -8,14 +8,12 @@ namespace SukkuShop.Migrations
         public override void Up()
         {
             AddColumn("dbo.Orders", "UserHints", c => c.String());
-            DropColumn("dbo.Orders", "Discount");
             DropColumn("dbo.Orders", "SpecialAddress");
         }
         
         public override void Down()
         {
             AddColumn("dbo.Orders", "SpecialAddress", c => c.Boolean(nullable: false));
-            AddColumn("dbo.Orders", "Discount", c => c.Decimal(nullable: false, precision: 18, scale: 2));
             DropColumn("dbo.Orders", "UserHints");
         }
     }
